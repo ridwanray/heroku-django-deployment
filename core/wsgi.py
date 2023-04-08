fb22f06f50bce3df32dd219226aa6c23cd256b5a
ridwanray <alabarise@gmail.com>
2023-04-08 23:13:55 +0100

    update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
+from decouple import config
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+environment = config('ENVIRONMENT')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings."+environment)
 
 application = get_wsgi_application()
